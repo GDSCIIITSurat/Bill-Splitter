c0838739a517c43acdb67c3129176d3a18099355
Zaid Bhimala <zaidm124@gmail.com>
2022-10-03 23:37:20 +0530

    heroku deploy

--- controllers/demo.js
+++ controllers/demo.js
@@ -1,6 +1,6 @@
 exports.getDemo = async (req,res,next)=>{
     try{
-        res.status(200).send("Welcome to Demo route");
+        res.status(200).send("Welcome to Demo route for backend");
     }catch(e){
         res.status(404).send("some error occured");
     }
