dcd1ba14bc707b614490967648897b6a20f9b4aa
Zaid Bhimala <zaidm124@gmail.com>
2022-10-04 10:09:10 +0530

    New PR Deploy check

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
 function App() {
   return (
+    <>
     <div className="App">
       Welcome to Bill Splitter Web app React version
     </div>
+    <div>
+      Demo for new PR
+    </div>
+    </>
   );
 }
 
