1b3887d52ea373780781749588ca2774639153b2
mandyval17 <mayurvalecha2603@gmail.com>
2022-10-05 22:31:59 +0530

    Added Login feature

--- routes/Routes.js
+++ routes/Routes.js
@@ -3,9 +3,11 @@ const router = express.Router()
 
 const {
   signUp,
+  logIn
 } = require('../controllers/Signup,login')
 
 router.route('/signup').post(signUp)
+router.route('/login').get(logIn)
 
 
 module.exports = router
