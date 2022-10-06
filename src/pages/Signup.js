f3a30cb5f21b329f63bc1c5c46be69af7d3f1d91
Ankit <kaushikankitsharma05@gmail.com>
2022-10-06 21:20:15 +0530

    Responsive Changes

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -22,11 +22,11 @@ function Signup(){
         setMail("")
     }
     return <div className="App">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH9BZD6iMPDJTb-obK1EgTuB9EIO4sZ6FVZs8US_OhiBmDizEwigp1ruMS9835BvSBTX0&usqp=CAU" alt=""/>
+        <img className="signup-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH9BZD6iMPDJTb-obK1EgTuB9EIO4sZ6FVZs8US_OhiBmDizEwigp1ruMS9835BvSBTX0&usqp=CAU" alt=""/>
 
         <form onSubmit={(e)=> e.preventDefault()} id="Signup-form">
 
-            <p id="Signup-p"><b>Signup</b></p>
+            <p ><b id="Signup-p">Signup</b></p>
 
             <TextField id="filled-basic" label="Email" onChange={(e)=>setMail(e.target.value)} value={Mail} type={"email"} variant="filled" />
 
