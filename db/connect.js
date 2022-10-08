72c536f6d845f6b7ca616998f4b2695b9cedaa37
vivek2293 <vivek22935@gmail.com>
2022-10-08 15:32:21 +0530

    Added Connection log

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  try {
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then(() => console.log("MongoDB is connected."))
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = connectDB;
