34ec7052fb5591bbfdb0f7ba917937abbfde0703
piqueblinder <preetkhatri07@gmail.com>
2022-10-18 16:58:10 +0530

    Authenticated the data entered on login page (HTML Branch)

--- js/login.js
+++ js/login.js
@@ -1,40 +1,46 @@
 const PostInfo = async (info) => {
-    const url =  "https://bill-splitter-backend-iiits.herokuapp.com/api/v1/login";
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    var raw = JSON.stringify(info);
-    
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-    };
-    
-    await fetch(url, requestOptions)
-      .then(res => {
-        console.log(res);
+  document.getElementById("button").disabled = true;
+  const url = "https://bill-splitter-backend-iiits.herokuapp.com/api/v1/login";
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-        if(res.status === 401){
-            window.alert(res.statusText);
-        }
-        else if(res.status === 200){
-            window.alert(res.statusText);
-        }
-        else if(res.status === 400){
-            window.alert(res.statusText); 
-        }
-        
-      })
-      .catch(error => console.log('error', error));
-    
+  var raw = JSON.stringify(info);
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+  };
+
+  await fetch(url, requestOptions)
+    .then(res => {
+      console.log(res);
+
+      if (res.status === 401) {
+        window.alert(res.statusText);
+        document.getElementById("button").disabled = false;
+        document.getElementById("password").value="";
+
+      }
+      else if (res.status === 200) {
+        window.alert(res.statusText);
+      }
+      else if (res.status === 400) {
+        window.alert(res.statusText);
+        document.getElementById("button").disabled = false;
+        document.getElementById("password").value="";
+      }
+
+    })
+    .catch(error => console.log('error', error));
+
 };
 
-document.querySelector("#loginForm").addEventListener("submit", (e)=> {
-    e.preventDefault(); 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
 
-    const info = { email : email, password};
-    PostInfo(info);
+document.querySelector("#loginForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const info = { email: email, password };
+  PostInfo(info);
 });
