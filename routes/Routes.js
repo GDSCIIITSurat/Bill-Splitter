5e84b525ed64b1a337dfb6fc8f1b9d055e64090e
Prasang023 <prasang023@gmail.com>
2022-10-22 23:41:28 +0530

    forgot password and reset password backend created

--- routes/Routes.js
+++ routes/Routes.js
@@ -3,11 +3,15 @@ const router = express.Router()
 
 const {
   signUp,
-  logIn
+  logIn,
+  forgotPassword,
+  resetPassword
 } = require('../controllers/auth')
 
 router.route('/signup').post(signUp)
 router.route('/login').post(logIn)
+router.route('/forgot-password').post(forgotPassword)
+router.route('/reset-password').post(resetPassword)
 
 
 module.exports = router
